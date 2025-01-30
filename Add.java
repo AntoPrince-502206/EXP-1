eca03a274437f100fa737d23a5754916eb2d0033
AntoPrince-502206 <anto.502206@sxcce.edu.in>
2025-01-30 13:52:12 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.sxcce.mavenproject1;
+
+/**
+ *
+ * @author IT20
+ */
+import java.util.*;
+public class Add 
+{
+    public static void main(String[] args) 
+    {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = a.nextInt();       
+        System.out.print("Enter the second number: ");
+        int num2 = a.nextInt();      
+        int sum = num1 + num2;       
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);        
+        a.close();
+    }
+}
