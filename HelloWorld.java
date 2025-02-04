43e53b775c358d6dd91021fde0b36c7bf3f12c82
AntoPrince-502206 <anto.502206@sxcce.edu.in>
2025-02-04 10:09:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.sxcce.mavenproject1;
+package com.sxcce.HelloWorld;
 
 /**
  *
  * @author IT20
  */
-public class Mavenproject1 {
+public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
